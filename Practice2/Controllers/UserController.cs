12b4d483bd07bb87b1bc116dc055904938bdcb37
denispimenov <pimenovdenis.prog@gmail.com>
2021-11-11 11:45:09 +0300

    Return user by id

--- Practice2/Controllers/UserController.cs
+++ Practice2/Controllers/UserController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Practice2.Models;
@@ -9,12 +8,20 @@ namespace Practice2.Controllers
     [Route("users")]
     public class UserController : ControllerBase
     {
-        private static readonly Dictionary<long, User> Users = new();
+        private static readonly Dictionary<long, User> Users = new()
+        {
+            [1] = new User(1, "Owner", Role.Admin)
+        };
 
         [HttpGet("{id:long}")]
-        public ActionResult Get(long id)
+        public ActionResult<User> Get(long id)
         {
-            throw new NotImplementedException();
+            if (Users.ContainsKey(id))
+            {
+                return Ok(Users[id]);
+            }
+
+            return NotFound();
         }
     }
 }
