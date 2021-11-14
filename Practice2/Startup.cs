9fbcb41610da1cf2e087ab86d6a8c01a16a0f1ad
denispimenov <pimenov@axlebolt.com>
2021-11-14 12:48:54 +0300

    Api key auth

--- Practice2/Startup.cs
+++ Practice2/Startup.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 
@@ -17,16 +19,49 @@ namespace Practice2
 
                 var filePath = Path.Combine(AppContext.BaseDirectory, $"{nameof(Practice2)}.xml");
                 c.IncludeXmlComments(filePath);
+
+                c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.ApiKey,
+                    In = ParameterLocation.Header,
+                    Name = "API-KEY",
+                    Description = "Api key auth",
+                });
+
+                var key = new OpenApiSecurityScheme()
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "ApiKey"
+                    },
+                    In = ParameterLocation.Header
+                };
+                var requirement = new OpenApiSecurityRequirement
+                {
+                    { key, new List<string>() }
+                };
+                c.AddSecurityRequirement(requirement);
             });
         }
 
         public void Configure(IApplicationBuilder app)
         {
             app.UseRouting();
-            app.UseEndpoints(endpoints => endpoints.MapControllers());
-
             app.UseSwagger();
             app.UseSwaggerUI();
+            app.Use(async (context, func) =>
+            {
+                if (!context.Request.Headers.TryGetValue("API-KEY", out var key) || !key.Equals("secret-api-key"))
+                {
+                    context.Response.StatusCode = 401;
+                    await context.Response.WriteAsJsonAsync(new ApiError(10, "Invalid api key"));
+                    return;
+                }
+
+                await func();
+            });
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 }
